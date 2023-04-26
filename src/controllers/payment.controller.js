a66b7973e63bc371f4e4ba581b694da12e784072
Shashank Rathore <shashank.rathore@facctum.com>
2023-04-27 00:12:26 +0530

    ADD: Payment and Wishlist

---
+++ src/controllers/payment.controller.js
@@ -0,0 +1,17 @@
+// import stripeService from '../services/payment.service'
+import * as stripService from '../services/payment.service';
+
+export const makePayment = async (req, res, next) => {
+  try {
+    const { amount, token, demoProduct } = req.body;
+    const charge = await stripService.createCharge(
+      amount,
+      token,
+      demoProduct,
+      res
+    );
+    res.json({ message: 'Payment successful', charge });
+  } catch (error) {
+    res.status(500).json({ message: 'Payment failed', error });
+  }
+};
